e9fa9386a3113b3119121ed4a65fca6f0df6d711
Nathan Walker <nmwalker360@gmail.com>
2016-10-09 09:48:57 -0700

    permission edits

--- package_delivery/db/schema.rb
+++ package_delivery/db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161004051116) do
-
-
-#ActiveRecord::Schema.define(version: 20161003091437) do
-#ActiveRecord::Schema.define(version: 20161001122317) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,10 +78,6 @@ ActiveRecord::Schema.define(version: 20161004051116) do
     t.text     "wcategory"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-  end
-
-  create_table "test", id: false, force: :cascade do |t|
-    t.text "name", null: false
   end
 
   create_table "testimonies", force: :cascade do |t|
